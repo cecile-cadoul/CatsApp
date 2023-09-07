26d141e13a5f6b8f817cfe90a7e0b39f008b5ea1
Cécile <37663334+cecile-cadoul@users.noreply.github.com>
2023-09-07 15:03:28 +0200

    Initial Commit

---
+++ CatsApp/CatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppApp.swift
+//  CatsApp
+//
+//  Created by Cecile on 07/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
